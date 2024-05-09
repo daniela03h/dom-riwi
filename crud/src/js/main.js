a9433f42968aa9f5588a543c9df4225bb33d5114
Daniela Londono <daniela0393h@gmail.com>
2024-05-09 18:38:30 -0500

    alert

--- crud/src/js/main.js
+++ crud/src/js/main.js
@@ -1,8 +1,23 @@
 import '../scss/styles.scss';
 import * as bootstrap from 'bootstrap'
-import Swal from 'sweetalert2'
 import { coders } from "../../public/data/database.js"
-import { index } from './oprations.js';
+import { create, index } from './oprations.js';
+import { alertSmallSuccess } from './alert.js';
 
 const tbody = document.querySelector("tbody")
+
+
+const name = document.getElementById("name")
+const lastName = document.getElementById("last-name")
+const email = document.getElementById("email")
+const form = document.querySelector("form")
+
 index(coders, tbody)
+
+form.addEventListener("submit", function(event) {
+    create(name, lastName, email, coders)
+    alertSmallSuccess("Saved")
+    form.reset()
+    index(coders, tbody)
+    event. preventDefault()
+})
